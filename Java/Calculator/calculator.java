0dea7f3f0c6562eb3e5e4a10626dc671f107ba5d
Stephen Morris <smorris11@gwu.edu>
2018-12-30 20:21:48 -0500

    All basic functionality for calculator

--- Java/Calculator/calculator.java
+++ Java/Calculator/calculator.java
@@ -10,36 +10,66 @@
 import java.util.*;
 
 public class calculator{
+   // holds the two operands
    public static double[] nums = new double[2];
 
    public static void main(String[] args){
       // Prints an opening statement so the user know the calculor has started
       System.out.println("Initializing calculator...");
       String choice = userInterface();
-      int swtch = 0;
+      int swtch = 0; // integer for the switch cases
       double result;
       // System.out.println(choice);
-      if(choice.equalsIgnoreCase("ADD")){
+      if(choice.equalsIgnoreCase("ADD") || choice.equalsIgnoreCase("A")){
+         swtch = 0;
+      }
+      else if(choice.equalsIgnoreCase("SUBTRACT") || choice.equalsIgnoreCase("S")){
          swtch = 1;
+      }
+      else if(choice.equalsIgnoreCase("MULTIPLY") || choice.equalsIgnoreCase("M")){
+         swtch = 2;
+      }
+      else if(choice.equalsIgnoreCase("DIVIDE") || choice.equalsIgnoreCase("D")){
+         swtch = 3;
       }
 
       switch(swtch){
-         case 1:
+         case 0:
             result = nums[0] + nums[1];
-            System.out.println("Output = " + result);
+            // System.out.println("Output = " + result);
+            printResult(nums[0], nums[1], result);
             break;
+         case 1:
+            result = nums[0] - nums[1];
+            printResult(nums[0], nums[1], result);
+            break;
+         case 2:
+            result = nums[0] * nums[1];
+            printResult(nums[0], nums[1], result);
+            break;
+         case 3:
+            result = (nums[0] / nums[1]);
+            printResult(nums[0], nums[1], result);
+            break;
+
       }
    }
 
    public static String userInterface(){
       System.out.println("What function do you want to perform (Add, Subtract, Mulitply, Divide)?");
+      // Gets the operation type
       Scanner scan = new Scanner(System.in);
       String func = scan.nextLine();
+      // Gets the two operands
       System.out.println("Enter your first number");
       nums[0] = (double)(scan.nextInt());
       System.out.println("Enter your second number");
       nums[1] = (double)(scan.nextInt());
 
       return func;
+   }
+
+   public static void printResult(double a, double b, double c){
+      System.out.printf("%f + %f = %f\n", a, b, c);
    }
 }
