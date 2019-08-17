05cc76400e08dd2809d88cd15ede93814d1e5a54
Stephen Morris <smorris11@gwu.edu>
2019-08-16 22:18:46 -0400

    Deleted repetative code and changed description

--- Java/binary.java
+++ Java/binary.java
@@ -1,8 +1,8 @@
 /*
-** This program is designed to convert 8 bit binary numbers to decimal
-** Number of bits could be expanded using for loop in decimalToBinary but the
-** number would have to be specified and passed into the loop and the number
-** would have to be inside int bounds.
+** This program is designed to convert any length binary numbers to decimal
+**
+** Need to design a way to make the the input number is binary and keep the
+** number inside integet bounds.
 **
 ** Designer: Stephen Morris
 ** Class: Freelance
@@ -51,49 +51,6 @@ public class binary{
          subtractBinary *= 10;
          addDecimal *= 2;
       }
-
-      /*
-      // Checks the 1's place for a 1
-      if((binary % 2) == 1){
-         decimal += 1;
-         binary -= 1;
-      }
-
-      if((binary % 20) == 10){
-         decimal += 2;
-         binary -= 10;
-      }
-
-      if((binary % 200) == 100){
-         decimal += 4;
-         binary -= 100;
-      }
-
-      if((binary % 2000) == 1000){
-         decimal += 8;
-         binary -= 1000;
-      }
-
-      if((binary % 20000) == 10000){
-         decimal += 16;
-         binary -= 10000;
-      }
-
-      if((binary % 200000) == 100000){
-         decimal += 32;
-         binary -= 100000;
-      }
-
-      if((binary % 2000000) == 1000000){
-         decimal += 64;
-         binary -= 1000000;
-      }
-
-      if((binary % 20000000) == 10000000){
-         decimal += 128;
-         binary -= 10000000;
-      }
-      */
 
       return decimal;
    }
