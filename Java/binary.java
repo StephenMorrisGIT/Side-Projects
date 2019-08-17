fa460472a26253fe6b4c1c4f2d9685baee12b181
Stephen Morris <smorris11@gwu.edu>
2019-08-16 22:15:54 -0400

    Can now take any length binary number. Also changed how to check the size of the input binary number; this still needs to be improved to check if the number is actually binary.

--- Java/binary.java
+++ Java/binary.java
@@ -7,29 +7,33 @@
 ** Designer: Stephen Morris
 ** Class: Freelance
 ** Date Created: Fri, July 26, 2019
-** Last Modified: Fri, July 26, 2019
+** Last Modified: Fri, August 16, 2019
 */
 
 import java.util.*;
 
 public class binary{
    public static void main(String[] args){
-      System.out.println("This program will convert an 8 bit binary number into decimal.");
+      System.out.println("This program will convert an 8 bit binary number into decimal.");  // Opening message
       int result = binaryToDecimal();
-      System.out.print(" = " + result + "\n");
+      System.out.print(" = " + result + "\n");  // Print result to screen
    }
 
    public static int binaryToDecimal(){
-      System.out.println("Please enter all eight digits with no spaces.");
-      // Initialize scanner
-      Scanner scan = new Scanner(System.in);
-      // Scan in user input
-      int binary = scan.nextInt();
-      // If the number exceeds eight 1's then the number will not calculate
-      if(binary > 11111111){
+      System.out.println("How many digits do you want to convert?");
+      Scanner scan = new Scanner(System.in); // Initalize scanner
+      int digits = scan.nextInt();  // Take in number of digits
+
+      System.out.println("Enter number with no spaces.");
+      int binary = scan.nextInt();// Scan in binary number, reusing 'scan'
+
+      // Makes sure the number is the correct length
+         // Need a better way to tell if the number is really just 1's and 0's
+      if(binary > (int)Math.pow(10, digits)){
          System.out.println("Number is too large. Please re-enter.");
          binary = scan.nextInt();
       }
+
       System.out.print(binary);
       int decimal = 0;
 
@@ -37,7 +41,7 @@ public class binary{
       int subtractBinary = 1; // We will subtract this number from our binary number every time the modulo hits
       int addDecimal = 1; // We add this to our decimal number every time the modulo hits
 
-      for(int i = 0; i < 8; i++){
+      for(int i = 0; i < digits; i++){
          if((binary % divisor) == subtractBinary){
             decimal += addDecimal;
             binary -= subtractBinary;
