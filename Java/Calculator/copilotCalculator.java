d43f89002216f96bb350f7c0aab2d515d4a5e4bc
StephenMorrisGIT <smorris11@gwu.edu>
2023-10-29 15:33:27 -0400

    Added logging functionality and file

--- Java/Calculator/copilotCalculator.java
+++ Java/Calculator/copilotCalculator.java
@@ -1,7 +1,18 @@
-public class calculator {
+import java.util.Scanner;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class copilotCalculator {
+    private static final String LOG_FILE = "calculator.log";
+
+    
+
     public static void main(String[] args) {
         String userInput = getUserInput();
-        System.out.println("User input: " + userInput);
+        log("User input: " + userInput);
     }
 
     public static String getUserInput() {
@@ -10,6 +21,19 @@ public class calculator {
         String input = scanner.nextLine();
         scanner.close();
         return input;
+    }
+
+
+    public static void log(String message) {
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        String timestamp = now.format(formatter);
+        try (PrintWriter out = new PrintWriter(new FileWriter(LOG_FILE, true))) {
+            out.print(timestamp + ": ");
+            out.println(message);
+        } catch (IOException e) {
+            System.err.println("Error writing to log file: " + e.getMessage());
+        }
     }
 }
 
